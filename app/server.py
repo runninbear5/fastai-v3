4e1f80d8117d0e3954e468ba2992cb04c9de34fc
Blake Lieber <blakelieber@gmail.com>
2019-07-08 22:43:12 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://drive.google.com/uc?export=download&id=1bcj051Ae2-ZOL_nBA0bGQZnokStJ1RI-'
-export_file_name = 'gesture-control.pth'
+export_file_name = 'stage-8.pth'
 
 classes = ['ok', '1', '2', '5']
 path = Path(__file__).parent
